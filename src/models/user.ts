d28790856410e9dfeff192881d14f1032b7c6441
Yamlak Fikru <118682315+Yamlak1@users.noreply.github.com>
2025-01-21 18:01:44 +0300

    Update user.ts

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,9 +1,22 @@
 import { Sequelize, DataTypes, Model } from "sequelize";
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./bot.db",
-});
+// Configure Sequelize to connect to MySQL
+const sequelize = new Sequelize(
+  "defaultdb",
+  "avnadmin",
+  "AVNS_nxv-10Ssw36Rw_bp0LN",
+  {
+    host: "market-place-bot-yamifikru4-4606.i.aivencloud.com",
+    port: 15107,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
 // Define the User model
 class User extends Model {
